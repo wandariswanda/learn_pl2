10bbc29cccbaa8e58284c91454676ce5d9eb22e5
wawan19 <wandariswanda1026@gmail.com>
2021-08-28 09:30:43 +0700

    penambahan base64_decode untuk setiap ID yang dikirim kan

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
     include 'koneksi.php';
     $db = new DbConnection(); 
     $conn = $db->getDbConnection();
-    $id = $_GET['mahasiswa_id'];
+    $id = base64_decode($_GET['mahasiswa_id']);
 
     $query = mysqli_query($conn, "DELETE from mahasiswa where id='$id'");
     header("location:latihan.php");
